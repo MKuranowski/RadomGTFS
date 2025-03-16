9ba4915506721c7a9f4f4a122a866a38f86a4385
Mikołaj Kuranowski <mkuranowski@gmail.com>
2025-03-16 09:56:27 +0100

    Bootstrap MultiFile impuls pipeline

--- radom_gtfs.py
+++ radom_gtfs.py
@@ -1,1 +1,27 @@
-print("Hello, world!")
+import argparse
+
+from impuls import App, HTTPResource, PipelineOptions
+from impuls.multi_file import IntermediateFeed, IntermediateFeedProvider, MultiFile
+
+
+class RadomIntermediateFileProvider(IntermediateFeedProvider[HTTPResource]):
+    def needed(self) -> list[IntermediateFeed[HTTPResource]]:
+        raise NotImplementedError("TODO")
+
+
+class RadomGTFS(App):
+    def prepare(
+        self,
+        args: argparse.Namespace,
+        options: PipelineOptions,
+    ) -> MultiFile[HTTPResource]:
+        return MultiFile(
+            options=options,
+            intermediate_provider=RadomIntermediateFileProvider(),
+            intermediate_pipeline_tasks_factory=lambda _: [],
+            final_pipeline_tasks_factory=lambda _: [],
+        )
+
+
+if __name__ == "__main__":
+    RadomGTFS().run()
