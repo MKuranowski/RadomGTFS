ab2a07585a518b74b75debd044c0928d4acd2058
Mikołaj Kuranowski <mkuranowski@gmail.com>
2025-03-16 10:35:59 +0100

    Add fares

--- radom_gtfs.py
+++ radom_gtfs.py
@@ -1,9 +1,10 @@
 import argparse
 import re
+from typing import cast
 
 import requests
-from impuls import App, HTTPResource, PipelineOptions
-from impuls.model import Date, FeedInfo
+from impuls import App, HTTPResource, PipelineOptions, Task
+from impuls.model import Date, FareAttribute, FeedInfo
 from impuls.multi_file import IntermediateFeed, IntermediateFeedProvider, MultiFile
 from impuls.tasks import AddEntity, ExecuteSQL, GenerateTripHeadsign, LoadGTFS
 
@@ -35,49 +36,91 @@ class RadomGTFS(App):
         return MultiFile(
             options=options,
             intermediate_provider=RadomIntermediateFileProvider(),
-            intermediate_pipeline_tasks_factory=lambda feed: [
-                LoadGTFS(feed.resource_name),
-                ExecuteSQL(
-                    task_name="FixAgency",
-                    statement=(
-                        "UPDATE agencies SET name = 'MZDiK Radom', url ='https://mzdik.pl/', "
-                        "fare_url = 'https://mzdik.pl/index.php?id=157'"
+            intermediate_pipeline_tasks_factory=lambda feed: cast(
+                list[Task],
+                [
+                    LoadGTFS(feed.resource_name),
+                    ExecuteSQL(
+                        task_name="FixAgency",
+                        statement=(
+                            "UPDATE agencies SET agency_id = '1', name = 'MZDiK Radom', "
+                            "url ='https://mzdik.pl/', "
+                            "fare_url = 'https://mzdik.pl/index.php?id=157'"
+                        ),
                     ),
-                ),
-                ExecuteSQL(
-                    task_name="DropFeedInfo",
-                    statement="DELETE FROM feed_info",
-                ),
-                AddEntity(
-                    task_name="AddFeedInfo",
-                    entity=FeedInfo(
-                        "Mikołaj Kuranowski",
-                        "https://mkuran.pl/gtfs/",
-                        "pl",
-                        feed.version,
+                    ExecuteSQL(
+                        task_name="DropFeedInfo",
+                        statement="DELETE FROM feed_info",
                     ),
-                ),
-                ExecuteSQL(
-                    task_name="FixRouteColor",
-                    statement=(
-                        "UPDATE routes SET "
-                        "color = CASE "
-                        "  WHEN short_name LIKE 'N%' THEN '000000' "
-                        "  ELSE 'E31E24' END, "
-                        "text_color = 'FFFFFF'"
+                    AddEntity(
+                        task_name="AddFeedInfo",
+                        entity=FeedInfo(
+                            "Mikołaj Kuranowski",
+                            "https://mkuran.pl/gtfs/",
+                            "pl",
+                            feed.version,
+                        ),
                     ),
-                ),
-                ExecuteSQL(
-                    task_name="DropTripHeadsign",
-                    statement="UPDATE trips SET headsign = ''",
-                ),
-                GenerateTripHeadsign(),
-                # TODO: Fix stop locations
-            ],
-            final_pipeline_tasks_factory=lambda _: [
-                # TODO: Add fares
-                # TODO: SaveGTFS
-            ],
+                    ExecuteSQL(
+                        task_name="FixRouteColor",
+                        statement=(
+                            "UPDATE routes SET "
+                            "color = CASE "
+                            "  WHEN short_name LIKE 'N%' THEN '000000' "
+                            "  ELSE 'E31E24' END, "
+                            "text_color = 'FFFFFF'"
+                        ),
+                    ),
+                    ExecuteSQL(
+                        task_name="DropTripHeadsign",
+                        statement="UPDATE trips SET headsign = ''",
+                    ),
+                    GenerateTripHeadsign(),
+                    # TODO: Fix stop locations
+                ],
+            ),
+            final_pipeline_tasks_factory=lambda _: cast(
+                list[Task],
+                [
+                    AddEntity(
+                        task_name="AddSingleJourneyFare",
+                        entity=FareAttribute(
+                            agency_id="1",
+                            id="single",
+                            price=3.60,
+                            currency_type="PLN",
+                            payment_method=FareAttribute.PaymentMethod.ON_BOARD,
+                            transfers=0,
+                            transfer_duration=None,
+                        ),
+                    ),
+                    AddEntity(
+                        task_name="AddOneHourFare",
+                        entity=FareAttribute(
+                            agency_id="1",
+                            id="one_hour",
+                            price=5.00,
+                            currency_type="PLN",
+                            payment_method=FareAttribute.PaymentMethod.ON_BOARD,
+                            transfers=None,
+                            transfer_duration=3600,
+                        ),
+                    ),
+                    AddEntity(
+                        task_name="AddOneDayFare",
+                        entity=FareAttribute(
+                            agency_id="1",
+                            id="one_day",
+                            price=14.00,
+                            currency_type="PLN",
+                            payment_method=FareAttribute.PaymentMethod.ON_BOARD,
+                            transfers=None,
+                            transfer_duration=86400,
+                        ),
+                    ),
+                    # TODO: SaveGTFS
+                ],
+            ),
         )
 
 
