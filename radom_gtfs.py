c9af3200cc1d884971a553f173f2a2b5619aea14
Mikołaj Kuranowski <mkuranowski@gmail.com>
2025-03-16 10:23:46 +0100

    Fix agency.txt & feed_info.txt

--- radom_gtfs.py
+++ radom_gtfs.py
@@ -1,21 +1,20 @@
 import argparse
 import re
-from typing import cast
 
 import requests
-from impuls import App, HTTPResource, PipelineOptions, Task
-from impuls.model import Date
+from impuls import App, HTTPResource, PipelineOptions
+from impuls.model import Date, FeedInfo
 from impuls.multi_file import IntermediateFeed, IntermediateFeedProvider, MultiFile
-from impuls.tasks import LoadGTFS
+from impuls.tasks import AddEntity, ExecuteSQL, LoadGTFS
 
 
 class RadomIntermediateFileProvider(IntermediateFeedProvider[HTTPResource]):
     def needed(self) -> list[IntermediateFeed[HTTPResource]]:
-        with requests.get("http://mzdik.radom.pl/?id=145") as r:
+        with requests.get("https://mzdik.pl/?id=145") as r:
             r.raise_for_status()
             return [
                 IntermediateFeed(
-                    resource=HTTPResource.get("http://mzdik.radom.pl" + m[1]),
+                    resource=HTTPResource.get("https://mzdik.pl" + m[1]),
                     resource_name=f"Rozklady-{m[2]}-{m[3]}-{m[4]}{m[5]}.zip",
                     version=f"{m[2]}-{m[3]}-{m[4]}{m[5]}",
                     start_date=Date(int(m[2]), int(m[3]), int(m[4])),
@@ -36,17 +35,32 @@ class RadomGTFS(App):
         return MultiFile(
             options=options,
             intermediate_provider=RadomIntermediateFileProvider(),
-            intermediate_pipeline_tasks_factory=lambda feed: cast(
-                list[Task],
-                [
-                    LoadGTFS(feed.resource_name),
-                    # TODO: Prettify agency
-                    # TODO: Fix feed_info.txt
-                    # TODO: Prettify routes
-                    # TODO: Fix trip headsigns
-                    # TODO: Fix stop locations
-                ],
-            ),
+            intermediate_pipeline_tasks_factory=lambda feed: [
+                LoadGTFS(feed.resource_name),
+                ExecuteSQL(
+                    task_name="FixAgency",
+                    statement=(
+                        "UPDATE agencies SET name = 'MZDiK Radom', url ='https://mzdik.pl/', "
+                        "fare_url = 'https://mzdik.pl/index.php?id=157'"
+                    ),
+                ),
+                ExecuteSQL(
+                    task_name="DropFeedInfo",
+                    statement="DELETE FROM feed_info",
+                ),
+                AddEntity(
+                    task_name="AddFeedInfo",
+                    entity=FeedInfo(
+                        "Mikołaj Kuranowski",
+                        "https://mkuran.pl/gtfs/",
+                        "pl",
+                        feed.version,
+                    ),
+                ),
+                # TODO: Prettify routes
+                # TODO: Fix trip headsigns
+                # TODO: Fix stop locations
+            ],
             final_pipeline_tasks_factory=lambda _: [
                 # TODO: Add fares
                 # TODO: SaveGTFS
